625ecf908be2c9aef68736ffd86ec67aa865b43f
tjpink <39979813+tjpink@users.noreply.github.com>
2021-12-27 18:57:10 +0800

    Update speechlist

--- openhouse_speechlist.py
+++ openhouse_speechlist.py
@@ -1,7 +1,7 @@
 class speechList():
     speech_00 = 'Please follow me to the zone 1 entrance'
-    speech_01 = 'Welcome to the school of engineering. Check out our course and project ' \
-                'booths and find out more about your dream courses'
-    speech_02 = "I am ready"
+    speech_01 = 'Welcome to the school of engineering. The entrance is on my left. ' \
+                'Please check out our course and project booths and find out more about your dream courses'
+    speech_02 = 'I am ready'
 
-    list = [speech_00, speech_01. speech_02]
+    list = [speech_00, speech_01, speech_02]
